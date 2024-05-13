b6bf93e8c5de49482c42731e728a70ee85d4b325
Khaled Hikmat <khaled.hikmat@gmail.com>
2024-05-13 11:41:06 -0500

    Cleanup

--- service/config/model.go
+++ service/config/model.go
@@ -1,11 +1,10 @@
 package config
 
 type Capturer struct {
-	AgentMode          string `json:"agentMode"`
-	MaxCameras         int    `json:"maxCameras"`
-	RecordingsFolder   string `json:"recordingsFolder"`
-	SamplesFolder      string `json:"samplesFolder"`
-	StorageDestination string `json:"storageDestination"`
+	AgentMode        string `json:"agentMode"`
+	MaxCameras       int    `json:"maxCameras"`
+	RecordingsFolder string `json:"recordingsFolder"`
+	SamplesFolder    string `json:"samplesFolder"`
 }
 
 type CloudStorage struct {

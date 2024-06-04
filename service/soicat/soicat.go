dbebd1b82fddbad7ce4d41ceb9064e6c95c64af4
Khaled Hikmat <khaled.hikmat@gmail.com>
2024-06-04 06:15:39 -0500

    Updtes to SOICAT Service to support AWS mode

--- service/soicat/soicat.go
+++ service/soicat/soicat.go
@@ -2,9 +2,11 @@ package soicat
 
 import (
 	"fmt"
+
+	"github.com/khaledhikmat/threat-detection-shared/service/config"
 )
 
-var cameras = []Camera{
+var localCameras = []Camera{
 	{
 		ID:                 "100",
 		Name:               "Camera1",
@@ -25,17 +27,95 @@ var cameras = []Camera{
 	},
 }
 
-func New() IService {
-	return &soicat{}
+var awsCameras = []Camera{
+	{
+		ID:                 "100",
+		Name:               "Camera1",
+		Region:             "Home_Office",
+		Location:           "B03W-E1",
+		Priority:           "ATM",
+		RtspURL:            "rtsp://admin:gooze_bumbs@192.168.1.206:554/cam/realmonitor?channel=1&subtype=0",
+		Analytics:          []string{"weapon", "fire"},
+		AlertTypes:         []string{"ccure", "snow", "pers", "slack"},
+		MediaIndexerTypes:  []string{"database", "elastic"},
+		Capturer:           "dead-capturer",
+		RetentionDays:      1,
+		DeepRetentionDays:  30,
+		CaptureWidth:       500,
+		CaptureHeight:      500,
+		MaxLengthRecording: 3,
+		Timezone:           "",
+	},
+	{
+		ID:                 "200",
+		Name:               "PHX AB JTRON QUAD CAM 1 (8240)",
+		Region:             "Phoenix",
+		Location:           "B01W-C1",
+		Priority:           "Normal",
+		RtspURL:            "rtsp://admin:gooze_bumbs@192.168.1.206:554/cam/realmonitor?channel=1&subtype=0",
+		Analytics:          []string{"weapon"},
+		AlertTypes:         []string{"ccure", "snow", "pers"},
+		MediaIndexerTypes:  []string{"elastic"},
+		Capturer:           "dead-capturer",
+		RetentionDays:      1,
+		DeepRetentionDays:  30,
+		CaptureWidth:       500,
+		CaptureHeight:      500,
+		MaxLengthRecording: 3,
+		Timezone:           "",
+	},
+	{
+		ID:                 "300",
+		Name:               "PLANO A ROOF NE PTZ (2160)",
+		Region:             "Plano",
+		Location:           "E03-FLOOR",
+		Priority:           "Normal",
+		RtspURL:            "rtsp://admin:gooze_bumbs@192.168.1.206:554/cam/realmonitor?channel=1&subtype=0",
+		Analytics:          []string{"weapon"},
+		AlertTypes:         []string{"ccure", "snow", "pers"},
+		MediaIndexerTypes:  []string{"elastic"},
+		Capturer:           "dead-capturer",
+		RetentionDays:      1,
+		DeepRetentionDays:  30,
+		CaptureWidth:       500,
+		CaptureHeight:      500,
+		MaxLengthRecording: 3,
+		Timezone:           "",
+	},
+	{
+		ID:                 "400",
+		Name:               "CENTS 10TH FLR E STAIR ENTRANCE (7528)",
+		Region:             "Charlotte",
+		Location:           "C-10W",
+		Priority:           "Normal",
+		RtspURL:            "rtsp://admin:gooze_bumbs@192.168.1.206:554/cam/realmonitor?channel=1&subtype=0",
+		Analytics:          []string{"weapon"},
+		AlertTypes:         []string{"ccure", "snow", "pers"},
+		MediaIndexerTypes:  []string{"elastic"},
+		Capturer:           "dead-capturer",
+		RetentionDays:      1,
+		DeepRetentionDays:  30,
+		CaptureWidth:       500,
+		CaptureHeight:      500,
+		MaxLengthRecording: 3,
+		Timezone:           "",
+	},
+}
+
+func New(cfgsvc config.IService) IService {
+	return &soicat{
+		CfgSvc: cfgsvc,
+	}
 }
 
 type soicat struct {
+	CfgSvc config.IService
 }
 
 func (s *soicat) UpdateCamera(c Camera) error {
-	for idx, camera := range cameras {
+	for idx, camera := range localCameras {
 		if camera.Name == c.Name {
-			ptr := &cameras[idx]
+			ptr := &localCameras[idx]
 			(*ptr).Capturer = c.Capturer
 			return nil
 		}
@@ -45,7 +125,10 @@ func (s *soicat) UpdateCamera(c Camera) error {
 }
 
 func (s *soicat) Cameras() ([]Camera, error) {
-	return cameras, nil
+	if s.CfgSvc.GetRuntimeEnv() == "local" {
+		return localCameras, nil
+	}
+	return awsCameras, nil
 }
 
 func (s *soicat) Finalize() {
